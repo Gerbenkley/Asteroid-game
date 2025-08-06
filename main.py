72b621c9bf12559865c8b981ede93a14a94fc238
Gerbenkley <65512404+Gerbenkley@users.noreply.github.com>
2025-08-06 21:04:48 +0200

    added group functionality

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
 from constants import *
-from player import Player
+from player import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
@@ -19,14 +21,15 @@ def main():
                 return
         
         dt = clock.tick(60) / 1000
-        print(dt)
-       
-        player.update(dt)
+
+        #MAIN LOOP
         screen.fill(0)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
+
         pygame.display.flip()
-
-
 
 
 if __name__ == "__main__":
